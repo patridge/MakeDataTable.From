1c45de5aec2134a8e6dc40ba2e60ced9870a15c3
Adam Patridge <patridge@gmail.com>
2012-03-20 09:16:24 -0600

    Initial import.

---
+++ MakeDataTable.From.Tests/MakeDataTableTests.cs
@@ -0,0 +1,95 @@
+﻿namespace MakeDataTable.From.Tests {
+    using NUnit.Framework;
+    using System.Data;
+    using System;
+
+    //class Program {
+    //    enum TestEnum {
+    //        y = 2,
+    //        z = 3
+    //    }
+    //    static void Main(string[] args) {
+    //        // enum value
+    //        DataTable test3 = MakeDataTable.From(new { x = (TestEnum?)null, y = TestEnum.y, z = TestEnum.z });
+    //        Console.ReadKey();
+    //    }
+    //}
+    public class MakeDataTableTests {
+        private enum TestEnumInt {
+            x = 1,
+            y = 2,
+            z = 3
+        }
+        private enum TestEnumLong : long {
+            x = 1,
+            y = 2,
+            z = 3
+        }
+        [TestFixture]
+        public class From {
+            [Test]
+            public void AnonymousObjectWithNullNullable_ReturnsDataTableWithDBNullValue() {
+                DataTable test = MakeDataTable.From(new { NullNullable = (int?)null });
+                Assert.IsNotNull(test);
+                Assert.AreEqual(1, test.Rows.Count);
+                Assert.AreEqual(typeof(int), test.Columns["NullNullable"].DataType);
+                Assert.AreEqual(DBNull.Value, test.Rows[0]["NullNullable"]);
+            }
+            [Test]
+            public void AnonymousObjectWithNullString_ReturnsDataTableWithDBNullValue() {
+                DataTable test = MakeDataTable.From(new { NullString = (string)null });
+                Assert.IsNotNull(test);
+                Assert.AreEqual(1, test.Rows.Count);
+                Assert.AreEqual(typeof(string), test.Columns["NullString"].DataType);
+                Assert.AreEqual(DBNull.Value, test.Rows[0]["NullString"]);
+            }
+            [Test]
+            public void AnonymousObjectWithEnum_ReturnsDataTableWithEnumUnderlyingValue() {
+                DataTable test = MakeDataTable.From(new { EnumValue = TestEnumInt.x });
+                Assert.IsNotNull(test);
+                Assert.AreEqual(1, test.Rows.Count);
+                Assert.AreEqual(typeof(int), test.Columns["EnumValue"].DataType);
+                Assert.AreEqual(1, test.Rows[0]["EnumValue"]);
+            }
+            [Test]
+            public void AnonymousObjectWithNullNullableEnum_ReturnsDataTableWithDBNullValue() {
+                DataTable test = MakeDataTable.From(new { NullEnum = (TestEnumInt?)null });
+                Assert.IsNotNull(test);
+                Assert.AreEqual(1, test.Rows.Count);
+                Assert.AreEqual(typeof(int), test.Columns["NullEnum"].DataType);
+                Assert.AreEqual(DBNull.Value, test.Rows[0]["NullEnum"]);
+            }
+            [Test]
+            public void AnonymousObjectWithLongEnum_ReturnsDataTableWithEnumUnderlyingValue() {
+                DataTable test = MakeDataTable.From(new { EnumValue = TestEnumLong.x });
+                Assert.IsNotNull(test);
+                Assert.AreEqual(1, test.Rows.Count);
+                Assert.AreEqual(typeof(long), test.Columns["EnumValue"].DataType);
+                Assert.AreEqual(1, test.Rows[0]["EnumValue"]);
+            }
+            [Test]
+            public void AnonymousObjectWithNullNullableLongEnum_ReturnsDataTableWithDBNullValue() {
+                DataTable test = MakeDataTable.From(new { NullEnum = (TestEnumLong?)null });
+                Assert.IsNotNull(test);
+                Assert.AreEqual(1, test.Rows.Count);
+                Assert.AreEqual(typeof(long), test.Columns["NullEnum"].DataType);
+                Assert.AreEqual(DBNull.Value, test.Rows[0]["NullEnum"]);
+            }
+            [Test]
+            public void ArrayOfAnonymousObjects_ReturnsDataTableWithRowForEach() {
+                DataTable test = MakeDataTable.From(new object[] {
+                    new { x = 1, y = "test1" },
+                    new { x = 2, y = "test2" }
+                });
+                Assert.IsNotNull(test);
+                Assert.AreEqual(2, test.Rows.Count);
+                Assert.AreEqual(typeof(int), test.Columns["x"].DataType);
+                Assert.AreEqual(typeof(string), test.Columns["y"].DataType);
+                Assert.AreEqual(1, test.Rows[0]["x"]);
+                Assert.AreEqual("test1", test.Rows[0]["y"]);
+                Assert.AreEqual(2, test.Rows[1]["x"]);
+                Assert.AreEqual("test2", test.Rows[1]["y"]);
+            }
+        }
+    }
+}
---
+++ MakeDataTable.From/MakeDataTable.cs
@@ -0,0 +1,59 @@
+﻿namespace MakeDataTable.From {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+
+    public static class MakeDataTable {
+        private static Type GetFinalType(Type type) {
+            var nullUnderlyingType = Nullable.GetUnderlyingType(type);
+            if (nullUnderlyingType != null) {
+                type = nullUnderlyingType;
+            }
+            if (type.IsEnum) {
+                type = Enum.GetUnderlyingType(type);
+            }
+            return type;
+        }
+        public static DataTable From(IEnumerable<object> objs) {
+            DataTable table = new DataTable();
+            if (objs == null) {
+                return new DataTable();
+            }
+
+            bool firstRun = true;
+            Dictionary<string, Type> columnData = new Dictionary<string, Type>();
+            List<Dictionary<string, object>> rowData = new List<Dictionary<string, object>>();
+            foreach (object rowObject in objs) {
+                Dictionary<string, object> row = new Dictionary<string, object>();
+                foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(rowObject)) {
+                    object value = descriptor.GetValue(rowObject);
+                    string name = descriptor.Name;
+                    Type type = descriptor.PropertyType;
+                    row.Add(name, value ?? DBNull.Value);
+                    if (firstRun) {
+                        columnData.Add(name, type);
+                    }
+                }
+                rowData.Add(row);
+                firstRun = false;
+            }
+
+            foreach (KeyValuePair<string, Type> column in columnData) {
+                table.Columns.Add(column.Key, GetFinalType(column.Value));
+            }
+            // Populate table
+            foreach (Dictionary<string, object> rowValues in rowData) {
+                DataRow row = table.NewRow();
+                foreach (KeyValuePair<string, object> kvp in rowValues) {
+                    row[kvp.Key] = kvp.Value;
+                }
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+        public static DataTable From(object obj) {
+            return From(new object[] { obj });
+        }
+    }
+}
